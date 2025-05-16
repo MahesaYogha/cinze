e654c547fffea208c7aa15a4cf458c8363da5bf5
MahesaYogha <mahestar03@gmail.com>
2025-05-17 00:22:13 +0700

    update

--- components/villa/videoSection.vue
+++ components/villa/videoSection.vue
@@ -22,7 +22,7 @@
             playsinline
             class="h-[500px] w-full md:h-full md:w-full object-cover object-center"
         >
-            <source src="/public/villa.webm" type="video/mp4" />
+            <source src="/public/villa.webm" type="video/webm" />
             Browser tidak mendukung video.
         </video>
     </div>
