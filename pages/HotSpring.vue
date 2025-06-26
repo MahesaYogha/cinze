db72ebc23f9910a9e07a3638553af0f2330925b5
MahesaYogha <mahestar03@gmail.com>
2025-06-26 22:25:12 +0700

    update

--- pages/HotSpring.vue
+++ pages/HotSpring.vue
@@ -1,18 +1,21 @@
 <template>
     <div class="bg-[#f3eee7] relative">
         <!-- Sticky Whatsapp Icon -->
-        <a 
-           href="https://api.whatsapp.com/send?phone=6285735368258&text=Halo%2C%20saya%20tertarik%20dengan%20layanan%20Hot%20Spring%20di%20Cinze%20Villa.%20Boleh%20dibantu%20info%20fasilitas%2C%20harga%2C%20dan%20cara%20booking-nya%3F"
-            target="_blank" 
-            rel="noopener noreferrer"
-            class="md:bottom-10 md:right-10 bottom-8 right-4 fixed z-10"
-        >
-            <img
-                src="~/assets/icons/whatsapp.svg"
-                alt="Chat WhatsApp"
-                class="w-[65px] md:w-20"
-            />
-        </a>
+         <div class="bg-[#f3eee7] relative">
+    <!-- Sticky Whatsapp Icon -->
+    <a 
+      href="https://wa.me/6285735368258?text=Hai%20kak%2C%20boleh%20tanya%20tentang%20Hot%20Spring%20di%20Cinze%20Villa%3F "
+      target="_blank" 
+      rel="noopener noreferrer"
+      class="md:bottom-10 md:right-10 bottom-8 right-4 fixed z-10"
+    >
+      <img
+        src="~/assets/icons/whatsapp.svg"
+        alt="Chat WhatsApp"
+        class="w-[65px] md:w-20"
+      />
+    </a>
+  </div>
 
         <!-- Navigator -->
         <HotspringNavigator :isScrolled="isScrolled" />
